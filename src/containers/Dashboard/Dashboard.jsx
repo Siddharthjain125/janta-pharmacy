3f33f1961f8b1e7d4ea2280441a20c5f72484a9b
siddharthjain125 <siddharthjain125@gmail.com>
2023-08-10 08:48:12 +0530

    Updated Dashboard and workflow

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,40 +1,46 @@
-import { Button, Card, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material';
-import React from 'react'
+import { Button, Card, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export function Dashboard() {
   return (
-    <Container sx={{ marginTop: '3rem' }}>
-    <Grid container spacing={3}>
-      <Grid item xs={12} md={6}>
-        <Typography variant="h4" gutterBottom>
-          Welcome to Janta Pharmacy
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Browse our wide range of medicines and healthcare products. With
-          quality products and fast delivery, we are Ujjain's most trusted
-          pharmacy.
-        </Typography>
-        <Button variant="contained" color="primary" size="large">
-          Shop Now
-        </Button>
+    <Container sx={{ marginTop: "3rem" }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h4" gutterBottom>
+            Welcome to Janta Pharmacy
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Browse our wide range of medicines and healthcare products. With quality products and
+            fast delivery, we are Ujjain's most trusted pharmacy.
+          </Typography>
+          <Button
+            component={Link}
+            to={`/products`}
+            variant="contained"
+            color="primary"
+            size="large"
+          >
+            Shop Now
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="100%"
+              image="/images/Banner.png"
+              alt="Pharmacy Image"
+            />
+            <CardContent>
+              <Typography variant="h6">Quick Delivery</Typography>
+              <Typography variant="body2">
+                Get your medications delivered to your doorstep in no time.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="100%"
-            image="/images/Banner.png"
-            alt="Pharmacy Image"
-          />
-          <CardContent>
-            <Typography variant="h6">Quick Delivery</Typography>
-            <Typography variant="body2">
-              Get your medications delivered to your doorstep in no time.
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
-  </Container>
+    </Container>
   );
 }
--- src/mockData/Products.js
+++ src/mockData/Products.js
@@ -2,19 +2,22 @@ export const mockData = [
   {
     title: "Rzole DSR",
     category: "Gut health",
+    type: "Medicines",
     mrp: 232,
     ourPrice: 100,
     packaging: "1 strip of 10 tablets each",
     searchable: ["Rabsesec DSR", "Rabimol DSR"],
     composition: {
-      "key1": "value1",
-      "key2": "value2"
+      pantaprazole: "40mg",
+      domperadon: "5mg",
     },
     description:
       "Rzole DSR Capsule is a combination medicine used to treat gastroesophageal reflux disease (Acid reflux) and peptic ulcer disease by relieving the symptoms of acidity such as heartburn, stomach pain, or irritation. It also neutralizes the acid and promotes easy passage of gas to reduce stomach discomfort.",
     image: "/images/Rzole.jpeg",
     productId: 1,
+    status: "available",
     //quantity: 0,
+    company: "",
   },
   {
     title: "Cipcal 500",
@@ -23,30 +26,30 @@ export const mockData = [
     searchable: ["Rabsesec DSR", "Rabimol DSR"],
     packaging: "1 strip of 10 tablets each",
     composition: {
-      "key1": "value1",
-      "key2": "value2"
+      key1: "value1",
+      key2: "value2",
     },
     description:
       "Cipcal 500 Tablet is a dietary supplement formulated with calcium and vitamin D3 that promotes bone and joint health. It helps in muscle contraction, muscle building and flexibility of joints. It may help improve bone health and lower osteoporosis risk.",
     image: "images/Cipcal.jpeg",
     productId: 2,
-   // quantity: 0,
+    // quantity: 0,
   },
   {
-    title: "Calciquick Gemz 3000 Vitamin Tablets",
+    title: "Calciquick Gemz 3000 Vitamin Tablets Take one dose only for adults and women ",
     mrp: 232,
     ourPrice: 100,
     packaging: "1 strip of 10 tablets each",
     searchable: ["Rabsesec DSR", "Rabimol DSR"],
     composition: {
-      "key1": "value1",
-      "key2": "value2"
+      key1: "value1",
+      key2: "value2",
     },
     description:
       "These tablets normalize calcium levels in the body by increasing the absorption of calcium and phosphate from the gastrointestinal tract. Calcitriol is also frequently used in the treatment of hypocalcaemia and osteoporosis. Calcium carbonate could also be taken as a nutritional supplement. Calcium is very important nutrient for the body, which helps the brain, bones, cells, and nerve to function properly. If there is not enough calcium in the blood, then the body will take it from the bones and weaken them.",
     image: "images/Calciquick.jpeg",
     productId: 3,
-   // quantity: 0,
+    // quantity: 0,
   },
   {
     title: "Limcee",
@@ -55,14 +58,14 @@ export const mockData = [
     packaging: "1 strip of 10 tablets each",
     searchable: ["Rabsesec DSR", "Rabimol DSR"],
     composition: {
-      "key1": "value1",
-      "key2": "value2"
+      key1: "value1",
+      key2: "value2",
     },
     description:
       "These tablets normalize calcium levels in the body by increasing the absorption of calcium and phosphate from the gastrointestinal tract. Calcitriol is also frequently used in the treatment of hypocalcaemia and osteoporosis. Calcium carbonate could also be taken as a nutritional supplement. Calcium is very important nutrient for the body, which helps the brain, bones, cells, and nerve to function properly. If there is not enough calcium in the blood, then the body will take it from the bones and weaken them.",
     image: "images/Limcee.jpeg",
     productId: 4,
-   // quantity: 0,
+    // quantity: 0,
   },
   {
     title: "Rabesec DSR",
@@ -71,13 +74,13 @@ export const mockData = [
     packaging: "1 strip of 10 tablets each",
     searchable: ["Rabsesec DSR", "Rabimol DSR"],
     composition: {
-      "key1": "value1",
-      "key2": "value2"
+      key1: "value1",
+      key2: "value2",
     },
     description:
       "These tablets normalize calcium levels in the body by increasing the absorption of calcium and phosphate from the gastrointestinal tract. Calcitriol is also frequently used in the treatment of hypocalcaemia and osteoporosis. Calcium carbonate could also be taken as a nutritional supplement. Calcium is very important nutrient for the body, which helps the brain, bones, cells, and nerve to function properly. If there is not enough calcium in the blood, then the body will take it from the bones and weaken them.",
     image: "images/Rzole.jpeg",
     productId: 5,
-   // quantity: 0,
-  }
+    // quantity: 0,
+  },
 ];
